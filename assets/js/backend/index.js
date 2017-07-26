b59d6c59f6e592a727ca5a37ffbfd5a62f5a9176
Karson <karsonzhang@163.com>
2017-07-26 14:30:11 +0800

    更换统计代码

--- assets/js/backend/index.js
+++ assets/js/backend/index.js
@@ -403,11 +403,5 @@ define(['jquery', 'bootstrap', 'backend', 'addtabs', 'adminlte', 'form'], functi
         }
     };
 
-    //此处为FastAdmin的统计代码,正式使用请移除
-    var s = document.createElement("script");
-    s.type = "text/javascript";
-    s.src = "https://hm.baidu.com/hm.js?b56e3ea9557853cc46f80d8c61c83f81";
-    $("head").append(s);
-
     return Controller;
 });
